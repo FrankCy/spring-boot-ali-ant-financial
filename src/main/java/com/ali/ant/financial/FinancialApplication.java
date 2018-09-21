642fd4d9c700edba7cbb20842ed79ef27ec4c26a
cyoung <cy880708@163.com>
2018-09-21 13:49:40 +0800

    添加启动程序

--- src/main/java/com/ali/ant/financial/FinancialApplication.java
+++ src/main/java/com/ali/ant/financial/FinancialApplication.java
@@ -1,5 +1,8 @@
 package com.ali.ant.financial;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @version 1.0
  * @description：
@@ -10,10 +13,11 @@ package com.ali.ant.financial;
  * @date: 2018/9/19 下午3:15
  * @mofified By:
  */
+@SpringBootApplication
 public class FinancialApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(FinancialApplication.class, args);
     }
 
 }
